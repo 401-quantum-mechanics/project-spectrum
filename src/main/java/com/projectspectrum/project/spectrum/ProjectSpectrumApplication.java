8177410320ec64290fab0c8789188c2db32bb550
Matt Stuhring <whereyouatmatt@gmail.com>
2019-08-30 16:24:56 -0700

    Initial commit with .gitignore

---
+++ src/main/java/com/projectspectrum/project/spectrum/ProjectSpectrumApplication.java
@@ -0,0 +1,13 @@
+package com.projectspectrum.project.spectrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpectrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpectrumApplication.class, args);
+	}
+
+}
