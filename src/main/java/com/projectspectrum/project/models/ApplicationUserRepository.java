4efd43dd9500cd99855d59b326e0bc427c119e9c
twardcox <t.ward.cox@gmail.com>
2019-09-03 12:52:03 -0700

    this better work

--- src/main/java/com/projectspectrum/project/models/ApplicationUserRepository.java
+++ src/main/java/com/projectspectrum/project/models/ApplicationUserRepository.java
@@ -3,6 +3,6 @@ package com.projectspectrum.project.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
-  public ApplicationUser findByUsername(String username);
+  public ApplicationUser findByEmail(String email);
   public ApplicationUser findById(long id);
 }
