55cb3450df66f8fbabd708dd3ad4981fad744828
twardcox <t.ward.cox@gmail.com>
2019-09-04 15:03:33 -0700

    code scrub first pass

--- src/main/java/com/projectspectrum/project/controllers/HomeController.java
+++ src/main/java/com/projectspectrum/project/controllers/HomeController.java
@@ -6,19 +6,10 @@ import com.projectspectrum.project.models.ApplicationUserRepository;
 import com.projectspectrum.project.models.UserIdea;
 import com.projectspectrum.project.models.UserIdeaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.view.RedirectView;
-
 import java.security.Principal;
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
