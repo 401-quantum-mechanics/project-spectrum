eaaee92da60275c81348a798dc3e8c3894ed19d8
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-09-04 15:58:33 -0700

    Same

--- src/main/java/com/projectspectrum/project/models/TeamUpRepository.java
+++
@@ -1,6 +0,0 @@
-package com.projectspectrum.project.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamUpRepository extends JpaRepository<TeamUp, Long> {
-}
